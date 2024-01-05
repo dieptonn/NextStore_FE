d475f1ec04e7f30cc641b478ce6a4864beddcd56
dieptonn <diep.bt194505@sis.hust.edu.vn>
2024-01-05 07:34:26 +0700

    update url

--- src/components/details/URL/URL.tsx
+++ src/components/details/URL/URL.tsx
@@ -4,7 +4,7 @@ export default function URL() {
     return (
         <div className={styles['url']}>
             <div className={styles['url-text']}>
-                Nextstore /Kompyuterlar va notebooklar /Laptop Notebooklar /Notebook Apple Macbook Pro 13&quot 2.4 2019 Intel core i5  DDR3 8 GB SSD 256 GB
+                Nextstore /Máy giặt /Máy giặt mới /Nhà phân phối Casper /Máy giặt Casper Inverter 8 kg WF-8VG1
             </div>
         </div>
     )
