91dbaba8fc23ec2d7d75bdea83ded5f67d977ea5
dieptonn <diep.bt194505@sis.hust.edu.vn>
2024-03-19 23:36:57 +0700

    update

--- src/components/chatbot/chatbot.tsx
+++ src/components/chatbot/chatbot.tsx
@@ -1,22 +1,33 @@
+// chatbot.tsx
 'use client'
 import styles from './styles.module.scss';
 import { useEffect } from 'react';
 
-declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            'df-messenger': any;
-        }
-    }
-}
+// declare global {
+//     namespace JSX {
+//         interface IntrinsicElements {
+//             'df-messenger': any;
+//         }
+//     }
+// }
 
 export default function Chatbot() {
 
     useEffect(() => {
         // Tạo một script element
         const script = document.createElement('script');
-        script.src = 'https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1';
+        script.type = 'text/javascript';
         script.async = true;
+        script.innerHTML = `
+            (function(d, m){
+                var kommunicateSettings = 
+                    {"appId":"2f57c1a909b1d305cd848e0975d059e0","popupWidget":true,"automaticChatOpenOnNavigation":true};
+                var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+                s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+                var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+                window.kommunicate = m; m._globals = kommunicateSettings;
+            })(document, window.kommunicate || {});
+        `;
 
         // Thêm script vào body của trang
         document.body.appendChild(script);
@@ -29,12 +40,6 @@ export default function Chatbot() {
 
     return (
         <div>
-            <df-messenger className={styles['chatbot']}
-                intent="WELCOME"
-                chat-title="NextChat"
-                agent-id="d19f3953-b7db-4251-928b-31a5ed9b901d"
-                language-code="en"
-            ></df-messenger>
         </div>
     )
 }
